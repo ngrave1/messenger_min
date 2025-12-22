26ac07d236d25dae820e80338b9358f06d3768b4
ngrave1 <ngrave@gmail.com>
2025-12-22 20:39:12 +0300

    ya_eblan

--- back_src/dependencies.py
+++
@@ -1,6 +0,0 @@
-from typing import Annotated
-from fastapi import Depends
-from sqlalchemy.orm import Session
-from orm_utils import get_session
-
-SessionDep = Annotated[Session, Depends(get_session)]
